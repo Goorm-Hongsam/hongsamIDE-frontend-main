3336d510d22f95914c7a5e75f9d314ed634c190f
summermong <2514o_o@naver.com>
2023-12-23 01:36:08 +0900

    DESIGN: 메인 화면 반응형 작업 완료

--- src/Pages/Main.jsx
+++ src/Pages/Main.jsx
@@ -3,19 +3,37 @@ import Styles from './Main.module.css';
 import { useNavigate } from 'react-router-dom';
 
 const Main = () => {
-  /* 입장 시 라우팅 내비게이터 */
   const navigate = useNavigate();
-  const handleCheck = () => {
+  const handleStart = () => {
     navigate('/question');
   };
 
   return (
     <div className={Styles.Main}>
-      <div className={Styles.subtitle}>Step for Developer</div>
-      <div className={Styles.title}>Hongsam IDE</div>
-      <button className={Styles.startingBtn} onClick={handleCheck}>
-        문제 풀이 시작
-      </button>
+      <div className={Styles.mainText}>
+        <div className={Styles.sub}>
+          <div className={Styles.subtitle}>Step for Developer</div>
+          <div className={Styles.desc}>개발자가 되기 위한 첫 걸음</div>
+        </div>
+        <div className={Styles.main}>
+          <div className={Styles.title}>Hongsam IDE</div>
+        </div>
+        <div className={Styles.button}>
+          <button className={Styles.startBtn} onClick={handleStart}>
+            시작하기
+          </button>
+        </div>
+        <div className={Styles.bottom}>
+          <span>made by Hongsam</span>
+          <br />
+          [FE] summermong / ganggyunggyu
+          <br />
+          [BE] seoyeoning / dlehddn
+        </div>
+      </div>
+      <div className={Styles.mainGif}>
+        <img className={Styles.gif} src={'/video/main.gif'} />
+      </div>
     </div>
   );
 };
