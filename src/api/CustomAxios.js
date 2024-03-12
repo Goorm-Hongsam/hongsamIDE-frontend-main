d5acd12f93bd32b3e12afd6d1e88d36e8d8f0936
summermong <2514o_o@naver.com>
2024-03-12 19:24:49 +0900

    FIX: token 지워지는 버그 debug

--- src/api/CustomAxios.js
+++ src/api/CustomAxios.js
@@ -5,11 +5,12 @@ const instance = () => {
     baseURL: import.meta.env.VITE_SERVER_URL,
   });
 
+  // 요청 전에 헤더에 JWT 토큰을 추가하는 인터셉터
   axiosInstance.interceptors.request.use(
     config => {
       const JWTtoken = localStorage.getItem('Authorization');
       if (JWTtoken) {
-        config.headers.Authorization = `Bearer ${JWTtoken}`;
+        config.headers.Authorization = `${JWTtoken}`;
       }
       return config;
     },
@@ -18,32 +19,25 @@ const instance = () => {
     }
   );
 
+  // 응답에 대한 인터셉터
   axiosInstance.interceptors.response.use(
     response => {
-      // 응답이 성공하면 그대로 반환
+      localStorage.setItem('Authorization', response.headers.authorization);
       return response;
     },
-    async error => {
+    error => {
       const originalRequest = error.config;
 
-      if (
-        error.response &&
-        error.response.status === 401 &&
-        !originalRequest._retry
-      ) {
-        // 토큰이 만료되었을 경우 한 번만 재시도
-        originalRequest._retry = true;
+      // 토큰 만료 시 401 에러 발생 시 재시도하는 부분
+      if (error.response && error.response.status === 401 && !isRetrying) {
+        isRetrying = true;
 
-        // 로컬 스토리지에서 토큰 가져오기
-        const JWTtoken = localStorage.getItem('Authorization');
-
-        if (JWTtoken) {
-          // 토큰이 있으면 헤더에 추가하여 재시도
-          originalRequest.headers.Authorization = `Bearer ${JWTtoken}`;
-          return axiosInstance(originalRequest);
-        }
+        return Promise.reject(error);
+      } else if (error.response && error.response.status === 406) {
+        // 토큰 만료 시 처리
+        localStorage.removeItem('Authorization');
+        return Promise.reject(error);
       }
-      // 토큰이 없거나, 재시도 중에도 실패하면 에러 반환
       return Promise.reject(error);
     }
   );
