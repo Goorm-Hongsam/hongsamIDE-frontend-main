8686096bb76d4c30d80e813cb7d7825b6a1b34d3
summermong <2514o_o@naver.com>
2024-03-12 18:49:26 +0900

    FIX: token 지워지는 버그 debug

--- src/api/CustomAxios.js
+++ src/api/CustomAxios.js
@@ -9,7 +9,7 @@ const instance = () => {
     config => {
       const JWTtoken = localStorage.getItem('Authorization');
       if (JWTtoken) {
-        config.headers.Authorization = `${JWTtoken}`;
+        config.headers.Authorization = `Bearer ${JWTtoken}`;
       }
       return config;
     },
@@ -20,42 +20,34 @@ const instance = () => {
 
   axiosInstance.interceptors.response.use(
     response => {
-      const JWTtoken = response.headers.Authorization;
-      localStorage.setItem('Authorization', JWTtoken);
-
+      // 응답이 성공하면 그대로 반환
       return response;
     },
-    error => {
+    async error => {
       const originalRequest = error.config;
 
-      if (error.response && error.response.status === 401) {
-        console.log('로그인을 해주세요.');
-      } else if (error.response && error.response.status === 406) {
-        // 토큰 만료 시 처리
-        // localStorage.removeItem('Authorization');
-        return Promise.reject(error);
+      if (
+        error.response &&
+        error.response.status === 401 &&
+        !originalRequest._retry
+      ) {
+        // 토큰이 만료되었을 경우 한 번만 재시도
+        originalRequest._retry = true;
+
+        // 로컬 스토리지에서 토큰 가져오기
+        const JWTtoken = localStorage.getItem('Authorization');
+
+        if (JWTtoken) {
+          // 토큰이 있으면 헤더에 추가하여 재시도
+          originalRequest.headers.Authorization = `Bearer ${JWTtoken}`;
+          return axiosInstance(originalRequest);
+        }
       }
+      // 토큰이 없거나, 재시도 중에도 실패하면 에러 반환
+      return Promise.reject(error);
     }
   );
 
-  // function getNewToken() {
-  //   return axiosInstance
-  //     .post(`/login-check`, { withCredentials: true })
-  //     .then(response => {
-  //       if (response.status === 200) {
-  //         localStorage.setItem('Authorization', response.headers.authorization);
-  //         return response.data;
-  //       } else {
-  //         console.log('엑세스 토큰 갱신에 실패했습니다.');
-  //         localStorage.removeItem('Authorization');
-  //         return Promise.reject('엑세스 토큰 갱신 실패');
-  //       }
-  //     })
-  //     .catch(error => {
-  //       alert('로그인을 다시 해주세요.');
-  //       return Promise.reject(error);
-  //     });
-  // }
   return axiosInstance;
 };
 
